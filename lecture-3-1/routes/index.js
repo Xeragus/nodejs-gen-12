13de2accd28954be934d4f80e430bcd934088052
Boban Sugareski <boban.s@slicelife.com>
2020-12-23 22:03:16 +0100

    Add code for lecture 4

--- lecture-3-1/routes/index.js
+++ lecture-3-1/routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-const players = [{ name: 'Auba' }, { name: 'Laca' }]
+// simulacija na baza
+const players = []
 
-router.get('/', (req, res) => {
-  res.render('index', { 
-    title: 'Players App',
-    players: players 
-  });
-});
+router
+      .get('/', (req, res) => {
+        res.render('index', { title: 'Players App', players });
+      })
+      .get('/create', (req, res) => {
+        res.render('create', { title: 'Create player' })
+      })
+      .get('/players/:id', (req, res) => {
+        const player = players.find(player => player.id == req.params.id)
+
+        res.render('edit', { title: 'Update player', player })
+      })
+      .post('/players', (req, res) => {
+        const player = { ...req.body, id: players.length + 1 }
+        players.push(player)
+        res.redirect('/')
+      })
 
 module.exports = router;
